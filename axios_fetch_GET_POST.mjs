99343c33e5309468a761eb72ca5c60a31ddfbe6b
Rawad Hossain <rawad.hossain00@gmail.com>
2024-09-08 00:15:15 +0600

    Update

--- axios_fetch_GET_POST.mjs
+++ axios_fetch_GET_POST.mjs
@@ -3,11 +3,13 @@ import axios from "axios";
 
 const app = express();
 
-GET => cannot send body as the second argument, second argument is for headers
+
+
+// GET => cannot send body as the second argument, second argument is for headers
 async function mainPost() {
     const res = await axios.post(
         "https://httpdump.app/dumps/f554bca6-460c-4c9d-9b6e-51f8c731a7da?a=2&b=45",
-        { username: "rwd", password: "rwd2003" },
+        { username: "rwd", password: "1234" },
         {
             headers: {
                 Authorization: "Bearer 1234567890",
@@ -18,6 +20,8 @@ async function mainPost() {
 
     console.log(res.data);
 }
+
+
 
 async function mainPost() {
     const res = await axios.get(
@@ -32,6 +36,9 @@ async function mainPost() {
 
     console.log(res.data);
 }
+
+
+
 
 // POST => cannot send body as the second argument, second argument is for headers
 //POST => for fetch
